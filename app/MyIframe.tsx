814b72ff03ba7b6b874a6ec163c874f2614a9140
purpurovvy <37051152+purpurovvy@users.noreply.github.com>
2024-01-17 12:47:13 +0100

    Update MyIframe.tsx

--- app/MyIframe.tsx
+++ app/MyIframe.tsx
@@ -3,6 +3,6 @@
 export const MyIframe = () => {
 
     return (
-        <iframe src="https://dainty-kheer-fd41bc.netlify.app/" width='1000px' height='1000px' allow="geolocation *; />
+        <iframe src="https://dainty-kheer-fd41bc.netlify.app/" width='1000px' height='1000px' allow="geolocation *" />
     )
 }
