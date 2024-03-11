b4124cedf3a22d38a88887c6ca7ce6876e7162fa
purpurovvy <purpurovvy@gmail.com>
2024-03-11 10:00:14 +0100

    added links

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
           <div style={{display:'flex', flexDirection:'column'}}>
-          <a href="https://www.google.com/maps/dir/?api=1&destination=52.6522,15.6129&origin=52.5257,16.852">Standard link</a>
-          <a href="geo:37.7749,-122.4194?q=37.8199,-122.4783">Link to android maps</a>
-          <a href="maps://?saddr=37.7749,-122.4194&daddr=37.8199,-122.4783">Link to iOS maps</a>
+          <a style={{marginTop:'16px'}} href="https://www.google.com/maps/dir/?api=1&destination=52.6522,15.6129&origin=52.5257,16.852"><button type="button">Standard link</button></a>
+          <a style={{marginTop:'16px'}} href="geo:37.7749,-122.4194?q=37.8199,-122.4783"><button type="button">Android maps</button></a>
+          <a style={{marginTop:'16px'}} href="maps://?saddr=37.7749,-122.4194&daddr=37.8199,-122.4783"><button type="button">iOS maps</button></a>
           </div>
       </div>
     </main>
