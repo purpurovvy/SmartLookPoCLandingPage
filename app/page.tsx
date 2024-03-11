5a6ff485f36872be27c28096c7d4fab0004e46f3
purpurovvy <purpurovvy@gmail.com>
2024-03-11 10:06:17 +0100

    added links

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,16 @@ export default function Home() {
       <div className={styles.description}>
           <div style={{display:'flex', flexDirection:'column'}}>
           <a style={{marginTop:'16px'}} href="https://www.google.com/maps/dir/?api=1&destination=52.6522,15.6129&origin=52.5257,16.852"><button type="button">Standard link</button></a>
-          <a style={{marginTop:'16px'}} href="geo:37.7749,-122.4194?q=37.8199,-122.4783"><button type="button">Android maps</button></a>
-          <a style={{marginTop:'16px'}} href="maps://?saddr=37.7749,-122.4194&daddr=37.8199,-122.4783"><button type="button">iOS maps</button></a>
+          <a style={{marginTop:'16px'}} href="geo:52.5257,16.852?q=52.6522,15.6129"><button type="button" >Android maps</button></a>
+          <a style={{marginTop:'16px'}} href="maps://?saddr=52.5257,16.852&daddr=52.6522,15.6129"><button type="button">iOS maps</button></a>
+          </div>
+      <br/>
+      <br/>
+      <br/>
+          <div style={{display:'flex', flexDirection:'column'}}>
+              <a style={{marginTop:'16px'}}  onClick={()=>window.location.href='https://www.google.com/maps/dir/?api=1&destination=52.6522,15.6129&origin=52.5257,16.852'} href="https://www.google.com/maps/dir/?api=1&destination=52.6522,15.6129&origin=52.5257,16.852"><button type="button">Standard link</button></a>
+              <a style={{marginTop:'16px'}} onClick={()=>window.location.href='geo:52.5257,16.852?q=52.6522,15.6129'} href="geo:52.5257,16.852?q=52.6522,15.6129"><button type="button" >Android maps</button></a>
+              <a style={{marginTop:'16px'}} onClick={()=>window.location.href='maps://?saddr=52.5257,16.852&daddr=52.6522,15.6129'} href="maps://?saddr=52.5257,16.852&daddr=52.6522,15.6129"><button type="button">iOS maps</button></a>
           </div>
       </div>
     </main>
