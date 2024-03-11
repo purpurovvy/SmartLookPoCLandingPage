97adc0e1f805c33dba40fe26cb0f6b0bf14cb024
purpurovvy <purpurovvy@gmail.com>
2024-03-11 09:54:40 +0100

    added links

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 import { MyIframe } from './MyIframe'
 
@@ -6,8 +5,11 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>My Iframed website:</p>
-        <MyIframe />
+          <div style={{display:'flex', flexDirection:'column'}}>
+          <a href="https://www.google.com/maps/dir/?api=1&destination=52.6522,15.6129&origin=52.5257,16.852">Standard link</a>
+          <a href="geo:37.7749,-122.4194?q=37.8199,-122.4783">Link to android maps</a>
+          <a href="maps://?saddr=37.7749,-122.4194&daddr=37.8199,-122.4783">Link to iOS maps</a>
+          </div>
       </div>
     </main>
   )
